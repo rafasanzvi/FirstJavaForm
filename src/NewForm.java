f019820518fbf9a80b71e468a48ca130d3da5bf8
rafasanzvi <rafasv41@gmail.com>
2022-11-27 17:45:32 +0100

    Initial commit

---
+++ src/NewForm.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class NewForm extends JFrame {
+
+    private JTextField tfFirstName;
+    private JTextField tfLastName;
+    private JButton bntOK;
+    private JButton btnClear;
+    private JLabel IbWelcome;
+    private JPanel mainPanel;
+
+    //Constructor
+    public NewForm() {
+        setContentPane(mainPanel);
+        setTitle("Welcome");
+        setSize(450, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        bntOK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String firstName = tfFirstName.getText();
+                String secondName = tfLastName.getText();
+                IbWelcome.setText("Welcome " + firstName + " " + secondName);
+            }
+        });
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tfFirstName.setText("");
+                tfLastName.setText("");
+                IbWelcome.setText("");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        NewForm newForm = new NewForm();
+    }
+}
